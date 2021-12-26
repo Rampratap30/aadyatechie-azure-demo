8bb23cd889f0dad8a116b2d67821c572baa3572b
Rampratap <rampratapsingh_kushwah@intuit.com>
2021-12-26 08:19:28 +0530

    first commit

---
+++ src/test/java/com/aadyatechie/azure/emo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadyatechie.azure.emo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
